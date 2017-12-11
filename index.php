45e461576a4d87be7cf6b02a31031c72d7deff4f
andrick santos <andricksantos1@gmail.com>
2017-12-11 16:19:16 -0600

    cambio de ruta

---
+++ index.php
@@ -0,0 +1,161 @@
+<?php
+  session_start();
+
+  // isset verifica si existe una variable o eso creo xd
+  if(isset($_SESSION['id'])){
+    header('location: controller/redirec.php');
+  }
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimal-ui">
+    <link rel="icon" href="img/as.png" type="image/icon">
+    <title>Login en PHP</title>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/sweetalert.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/arsm.css">
+
+  </head>
+  <body>
+      <nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="index.php">Andrick Santos</a>
+    </div>
+
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      
+      
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="index.php">Inicio</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Contacto <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="https://andricksantos.github.io/" target="_blank">WebSite</a></li>
+            <li><a href="https://github.com/andricksantos" target="_blank">Github</a></li>
+            <li><a href="mailto:andricksantos1@gmail.com" target="_blank">Email</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#" class="disable center">ARSM</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+    <!-- Formulario Login -->
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12 col-md-4 col-md-offset-4">
+          
+          <div class="spacing-1"></div>
+
+          <!-- Estructura del formulario -->
+          <fieldset>
+
+            <legend class="center">Login</legend>
+
+            <!-- Caja de texto para usuario -->
+            <label class="sr-only" for="user">Usuario</label>
+            <div class="input-group">
+              <div class="input-group-addon"><i class="fa fa-user"></i></div>
+              <input type="text" class="form-control" id="user" placeholder="Ingresa tu usuario">
+            </div>
+
+            <!-- Div espaciador -->
+            <div class="spacing-2"></div>
+
+            <!-- Caja de texto para la clave-->
+            <label class="sr-only" for="clave">Contraseña</label>
+            <div class="input-group">
+              <div class="input-group-addon"><i class="fa fa-lock"></i></div>
+              <input type="password" autocomplete="off" class="form-control" id="clave" placeholder="Ingresa tu contraseña">
+            </div>
+
+            <!-- Animacion de load (solo sera visible cuando el cliente espere una respuesta del servidor )-->
+            <div class="row" id="load" hidden="hidden">
+              <div class="col-xs-4 col-xs-offset-4 col-md-2 col-md-offset-5">
+                <img src="img/load.gif" width="100%" alt="">
+              </div>
+              <div class="col-xs-12 center text-accent">
+                <span>Validando información...</span>
+              </div>
+            </div>
+            <!-- Fin load -->
+
+            <!-- boton #login para activar la funcion click y enviar el los datos mediante ajax -->
+            <div class="row">
+              <div class="col-xs-8 col-xs-offset-2">
+                <div class="spacing-2"></div>
+                <button type="button" class="btn btn-primary btn-block" name="button" id="login">Iniciar sesion</button>
+              </div>
+            </div>
+
+            <section class="text-accent center">
+              <div class="spacing-2"></div>
+              
+              <p>
+                No tienes una cuenta? <a href="registro.php"> Registrate!</a>
+              </p>
+            </section>
+
+          </fieldset>
+        </div>
+      </div>
+    </div>
+
+    <!-- / Final Formulario login -->
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+    <footer>
+    <div class="arsm center" id="copyright">
+      Copyright&copy; 2017 - Page created by 
+      <a class="as" target="_blank" href="https://andricksantos.github.io">-
+      Andrick Santos</a> - All rights reserved</div>
+      </div>
+    </div>
+
+
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/sweetalert.min.js"></script>
+    <script src="js/operaciones.js"></script>
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+        ga('create', 'UA-103804510-1', 'auto');
+        ga('send', 'pageview');
+  </script>
+  <!--<script type="text/javascript">
+      document.oncontextmenu = function(){return false;}
+  </script>-->
+  </body>
+</html>
